0518fc028b7bade110432071a8e98c883b2a8daa
aria ortko <megymikei@gmail.com>
2022-05-26 18:57:20 +0300

    feat: task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
 
+const curPath = path.join(__dirname, 'files');
+
+fsPromises.mkdir(path.join(__dirname, 'files-copy'), {recursive: true} );
+
+fs.readdir(curPath, { withFileTypes: true }, (err, files) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    files.forEach((file) => {
+        if (file.isFile()) {
+            const pathToFile = path.join(curPath, file.name);
+            fsPromises.copyFile(pathToFile, path.join(__dirname, 'files-copy', file.name));
+
+        }
+    });
+});
